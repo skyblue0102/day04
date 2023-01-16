1d7a65c4b2a38bf79585b1df0f847e293936de66
kangmunju <ikmj0871@gmail.com>
2023-01-16 18:28:27 +0900

    5-8

--- day03_mission.py
+++ day03_mission.py
@@ -1,8 +1,7 @@
-#5.7
-
+#5.8
 a = 'duck'
 b = 'gourd'
 c = 'spitz'
-print('{}y Mc{}face'.format(a.capitalize(), a.capitalize()))
-print('{}y Mc{}face'.format(b.capitalize(), b.capitalize()))
-print('{}y Mc{}face'.format(c.capitalize(), c.capitalize()))
+print(f'{a.capitalize()}y Mc{a.capitalize()}face')
+print(f'{b.capitalize()}y Mc{b.capitalize()}face')
+print(f'{c.capitalize()}y Mc{c.capitalize()}face')
