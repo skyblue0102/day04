a3debe438e170455bac885e3609380fdc7fd4ad4
kangmunju <ikmj0871@gmail.com>
2023-01-16 20:38:58 +0900

    6-2

--- day03_01.py
+++ day03_01.py
@@ -1,25 +1,5 @@
-#input 2 numbers
+# ex. 6-1
 
-# start_end = input("start and end number : ").split()
-# print(start_end)
-# print(int(start_end[0]), int(start_end[1]))
+for k in [3, 2, 1, 0]:
+    print(k)
 
-#prime_number
-start = int(input("start number : "))
-end = int(input("end number : "))
-print(start, end)
-
-if end < start:
-    start, end = end, start
-
-for i in range(start, end+1):
-    # is_prime = True
-
-    if i <= 1:
-        continue
-    for k in range(2, i):
-        if  i % k ==0:
-        # is_prime = False
-            break
-    else:
-        print(i, end=' ')
