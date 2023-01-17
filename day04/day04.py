b95d2278d459a39b0c2397c5aaaa2f8662d3251c
kangmunju <ikmj0871@gmail.com>
2023-01-17 10:57:34 +0900

    list

--- day04/day04.py
+++ day04/day04.py
@@ -1,24 +1,36 @@
-#tuple
+#list
+big_bang = ['GD', '태양', '탑', '대성', '승리']
+exo = ['백현', '첸']
+# big_bang.append('인하')
+big_bang.insert(1, '인하')
+# print(big_bang*3)
+# exo.extend(big_bang)
+# exo += big_bang
+exo.append(big_bang)
+print(exo)
+print(exo[2][2])  # 태양
+print(exo[-1][-4])
 
-a = 'harry',
-b = ('harry',)
-c = () #empty tuple
-d = 'harry', 'ron'  # packing
-e = ('hermione')  # string
-f = ('harry', 'ron')  # packing
-g = ('hermione', )  # string
-print(g, id(g))
-g = g + f
-print(g, id(g))
-print(g)
-print(g+f)
-print(f[1])
-x, y = f  #unpacking ('harry', 'ron')
-print(y)
+exo[-2] = '시우민'
+print(exo)
+# print(exo.pop()) #빅뱅 pop
+print(exo[2].pop()) #빅뱅 pop
+print(exo)
+print(exo[2].pop(-2)) #빅뱅 pop
+print(exo)
+del exo[2][-1] #대성 삭제
+print(exo)
+# exo.remove('인하')  (작동안됨)
+# print(exo)
+exo[2].remove('인하')
+print(exo)
 
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
+# scores = ('B+', 'A+', 'C+')
+# print(scores[1], scores[2])
+# temp = list(scores)
+# temp[1] = 'C+'
+# temp[2] = 'A+'
+# scores = tuple(temp)
+# print(scores[1], scores[2])
+
+
